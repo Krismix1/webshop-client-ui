8565f9fe6a2776a1e78cfb508d81befc7d672a26
Cristian Betivu <cbetivu@gmail.com>
2018-03-16 10:35:00 +0100

    Create components for navbar and search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
