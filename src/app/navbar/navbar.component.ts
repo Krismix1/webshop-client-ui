d98e669997af627a6acd6a05beb45c3edd9d815e
Cristian Betivu <cbetivu@gmail.com>
2018-04-06 10:30:56 +0200

    Display side shop cart

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgRedux } from '@angular-redux/store';
+import { IAppState } from './../store/store';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  private quantity: number = 0;
+
+  constructor(private ngRedux: NgRedux<IAppState>) { }
 
   ngOnInit() {
+    this.ngRedux.select(state => state.cart)
+      .subscribe(cart => this.quantity = cart.items.map(item => item.quantity).reduce((a, b) => a + b, 0););
+
   }
 
 }
